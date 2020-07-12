9c11c749280d207c80e8a9c4c575b5ed5ffae8bf
vincentc48 <vincent.cai48@gmail.com>
2020-07-12 15:57:45 -0400

    moved serving static assets to after the api and auth routers

--- server.js
+++ server.js
@@ -14,18 +14,7 @@ var jsonParser = bodyParser.json();*/
 app.use(express.json())
 
 
-if (process.env.NODE_ENV ==="production"){
-    app.use(express.static('client/build'))
-    app.get('*',(req,res)=>{
-        res.sendFile(path.resolve(__dirname,"client","build","index.html"));
-    })
-    console.log(process.env.NODE_ENV)
-}
-
 mongoose.connect(process.env.MONGO_URL,{useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true},()=>console.log("connected to MongoDB"))
-
-const port = process.env.PORT || 5000
-
 
 //if doesn't work, uncomment the json parse and add jsonParse as second param
 app.post('/addhome',(req,res)=>{
@@ -34,6 +23,16 @@ app.post('/addhome',(req,res)=>{
 
 app.use('/api',router)
 app.use('/auth',authrouter)
+
+if (process.env.NODE_ENV ==="production"){
+    app.use(express.static('client/build'))
+    app.get('*',(req,res)=>{
+        res.sendFile(path.resolve(__dirname,"client","build","index.html"));
+    })
+    console.log(process.env.NODE_ENV)
+}
+
+const port = process.env.PORT || 5000
 
 app.listen(port,()=>{
     console.log('Server running on port: '+ port)
